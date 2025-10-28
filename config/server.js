acdf76f34affedb91b65547e20b7c16b3b5ff26c
Clayton Wingfield <80126513+claytonwingfield@users.noreply.github.com>
2025-10-28 18:04:24 -0500

    dashboard

--- config/server.js
+++ config/server.js
@@ -1,6 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  // === THIS IS THE FIX ===
+  // Tells Strapi its public URL (Render provides RENDER_EXTERNAL_URL automatically)
+  url: env('https://backend-scorecard.onrender.com/admin this is my external url', 'http://localhost:1337'), 
+  // =======================
   app: {
     keys: env.array('APP_KEYS'),
   },
