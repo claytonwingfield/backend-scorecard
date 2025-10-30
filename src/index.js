c5ca128ced1dfaf7b941fb79572e6b594bad933a
Clayton Wingfield <80126513+claytonwingfield@users.noreply.github.com>
2025-10-30 17:49:48 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,16 @@ module.exports = {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register({ strapi }) {
+    // ==========================================================
+    // THIS IS OUR TEST
+    // ==========================================================
+    console.log("==========================================================");
+    console.log("✅✅✅ TEST LOG: Strapi 'register' function RAN. ✅✅✅");
+    console.log("If you see this log, your deploy was SUCCESSFUL.");
+    console.log("==========================================================");
+    // ==========================================================
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
